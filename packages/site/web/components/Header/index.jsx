71d3f893131efb8207eaa2e208484033daf152b1
zhixiangwang <wang.zhixiang100@iwhalecloud.com>
2021-09-30 10:11:14 +0800

    upd: 注释搜索功能

--- packages/site/web/components/Header/index.jsx
+++ packages/site/web/components/Header/index.jsx
@@ -4,40 +4,40 @@ import { IntlProvider, FormattedMessage } from 'react-intl';
 import { Icon, Popup, Radio } from 'zson-mobile';
 import { Dropdown, Menu } from 'zarm-web';
 import classnames from 'classnames';
-import docsearch from 'docsearch.js';
+// import docsearch from 'docsearch.js';
 import MenuComponent from '@/web/components/Menu';
-import Events from '@/utils/events';
+// import Events from '@/utils/events';
 import Context from '@/utils/context';
 import Locales from '@/locale';
 import pkg from '@zson-mobileDir/package.json';
-import 'docsearch.js/dist/cdn/docsearch.min.css';
+// import 'docsearch.js/dist/cdn/docsearch.min.css';
 import './style.scss';
 import 'zson/style/entry';
 
-const initDocSearch = () => {
-  docsearch({
-    apiKey: '0b658907591674d71e4d8664894fe887',
-    indexName: 'zson',
-    inputSelector: '.search input',
-    debug: false,
-  });
-};
+// const initDocSearch = () => {
+//   docsearch({
+//     apiKey: '0b658907591674d71e4d8664894fe887',
+//     indexName: 'zson',
+//     inputSelector: '.search input',
+//     debug: false,
+//   });
+// };
 
 const Icons = Icon.createFromIconfont('//at.alicdn.com/t/font_1340918_lpsswvb7yv.js');
 
 const Header = ({ children }) => {
-  const searchInput = useRef();
+  // const searchInput = useRef();
   const location = useLocation();
   const [locale, setLocale] = useState(window.localStorage.locale || 'zhCN');
   const [menu, toggleMenu] = useState(false);
   const [dropdown, setDropdown] = useState(false);
   const currentPageKey = location.pathname.split('/')[1] || '/';
 
-  const keyupEvent = (event) => {
-    if (event.keyCode === 83 && event.target === document.body) {
-      searchInput.current.focus();
-    }
-  };
+  // const keyupEvent = (event) => {
+  //   if (event.keyCode === 83 && event.target === document.body) {
+  //     searchInput.current.focus();
+  //   }
+  // };
 
   const activeClassName = (keys) => {
     return classnames({
@@ -61,21 +61,21 @@ const Header = ({ children }) => {
       link: '#/design',
       title: <FormattedMessage id="app.home.nav.resources" />,
     },
-    // { key: 'gitee', link: 'https://zarm.gitee.io', title: '国内镜像' },
+    { key: 'gitee', link: 'https://zson-mobile.gitee.io', title: '国内镜像' },
   ];
 
   if (document.location.host.indexOf('gitee') > -1 || locale === 'enUS') {
     NAV_ITEMS.pop();
   }
 
-  useEffect(() => {
-    Events.on(document, 'keyup', keyupEvent);
-    initDocSearch();
+  // useEffect(() => {
+  //   Events.on(document, 'keyup', keyupEvent);
+  //   initDocSearch();
 
-    return () => {
-      Events.off(document, 'keyup', keyupEvent);
-    };
-  }, []);
+  //   return () => {
+  //     Events.off(document, 'keyup', keyupEvent);
+  //   };
+  // }, []);
   const menuRender = currentPageKey !== '/' && (
     <div className="header-icon header-icon-menu">
       {currentPageKey !== 'design' && (
@@ -107,7 +107,7 @@ const Header = ({ children }) => {
               ))}
               <Menu.Item>
                 <a
-                  href="https://github.com/ZhongAnTech/zarm"
+                  href="https://github.com/zson-mobile/zson-mobile.github.io"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -137,12 +137,12 @@ const Header = ({ children }) => {
             </div>
             {moreRender}
             <nav>
-              <div className="search">
-                <Icon type="search" />
-                <FormattedMessage id="app.home.nav.search">
+              {/* <div className="search"> */}
+                {/* <Icon type="search" /> */}
+                {/* <FormattedMessage id="app.home.nav.search">
                   {(txt) => <input placeholder={txt} ref={searchInput} />}
-                </FormattedMessage>
-              </div>
+                </FormattedMessage> */}
+              {/* </div> */}
               <ul>
                 {NAV_ITEMS.map((item) => (
                   <li key={item.key}>
@@ -168,7 +168,7 @@ const Header = ({ children }) => {
               </div>
               <a
                 className="github"
-                href="https://github.com/13328941960/wzx"
+                href="https://github.com/zson-mobile/zson-mobile.github.io"
                 target="_blank"
                 rel="noopener noreferrer"
               >
